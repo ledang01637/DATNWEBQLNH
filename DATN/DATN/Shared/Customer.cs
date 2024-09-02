4b80389b36cb4d3dd1a685bf33dea7a5a8269d0f
nghiaDEV <149445948+nghiamhhpc05062@users.noreply.github.com>
2024-09-02 15:16:19 +0700

    API
    version_1.0

---
+++ DATN/DATN/Shared/Customer.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATN.Shared
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public bool IsDeleted { get; set; }
+        public int AccountId { get; set; }
+        public decimal TotalRewardPoint { get; set; }
+
+        public Account Accounts { get; set; }
+        public ICollection<Order> Orders { get; set; }
+        public RewardPointe RewardPoints { get; set; }
+        public ICollection<CustomerVoucher> CustomerVouchers { get; set; }
+    }
+}
