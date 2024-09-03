a541fa0bf2f149c158e6fcddf293327524094710
DangLH <ledang01637@gmail.com>
2024-09-03 20:04:28 +0700

    checkIpWifi

---
+++ DATN/DATN/Server/Service/NetworkService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.NetworkInformation;
+
+namespace DATN.Server.Service
+{
+    public class NetworkService
+    {
+        public string GetWifiIPAddress()
+        {
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 && ni.OperationalStatus == OperationalStatus.Up)
+                {
+                    var ipProperties = ni.GetIPProperties();
+                    foreach (var address in ipProperties.UnicastAddresses)
+                    {
+                        if (address.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            return address.Address.ToString();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
