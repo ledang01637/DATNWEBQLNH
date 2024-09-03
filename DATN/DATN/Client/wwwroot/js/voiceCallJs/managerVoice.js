dec5451c24d88003cd4b5d21ca9cc9fe4ad130ab
DangLH <ledang01637@gmail.com>
2024-09-03 20:04:28 +0700

    checkIpWifi

---
+++ DATN/DATN/Client/wwwroot/js/voiceCallJs/managerVoice.js
@@ -0,0 +1,9 @@
+﻿var localVideo = document.getElementById('localVideo');
+var remoteVideo = document.getElementById('remoteVideo');
+var btn = document.getElementById('answerCallButton');
+
+btn.addEventListener('click', function () {
+    localVideo.muted = false;
+    remoteVideo.muted = false;
+    localVideo.play();
+});
--- DATN/DATN/Server/Startup.cs
+++ DATN/DATN/Server/Startup.cs
@@ -1,3 +1,4 @@
+using DATN.Server.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,7 @@ namespace DATN.Server
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddSingleton<NetworkService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
