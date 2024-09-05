81acdcb627e5f91dd0c7ad18d9a8d58d876fb53d
DangLH <ledang01637@gmail.com>
2024-09-05 13:09:28 +0700

    add movetable

---
+++ DATN/DATN/Client/wwwroot/js/table.js
@@ -0,0 +1,21 @@
+﻿function MoveTable() {
+    var sortableFloor1 = document.getElementById('sortable-floor1');
+    console.log(sortableFloor1);  // In ra để kiểm tra phần tử có tồn tại không
+
+    if (sortableFloor1) {
+        new Sortable(sortableFloor1, {
+            animation: 150,
+            ghostClass: 'sortable-ghost',
+            handle: '.grid-item'
+        });
+    }
+
+    var sortableFloor2 = document.getElementById('sortable-floor2');
+    if (sortableFloor2) {
+        new Sortable(sortableFloor2, {
+            animation: 150,
+            ghostClass: 'sortable-ghost',
+            handle: '.grid-item'
+        });
+    }
+}
