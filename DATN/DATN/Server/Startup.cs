7898758cabac7d88dbdce49e14a9a917d00b11b5
metmoiquatroi <vinhlqpc05646@fpt.edu.vn>
2024-09-08 18:56:56 +0700

    xong dang ky dang nhap chua xac thuc token

--- DATN/DATN/Server/Startup.cs
+++ DATN/DATN/Server/Startup.cs
@@ -1,6 +1,5 @@
-using DATN.Server.Data;
-using DATN.Server.Service;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -8,8 +7,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using System.Linq;
 using System.Text;
-using System;
+using DATN.Server.Data;
+using DATN.Server.Service;
 
 namespace DATN.Server
 {
@@ -30,32 +31,29 @@ namespace DATN.Server
             services.AddControllersWithViews();
             services.AddRazorPages();
 
-            services.AddScoped<AccountService>();
-            services.AddScoped<CategoryService>();
-            services.AddScoped<CustomerService>();
-            services.AddScoped<CustomerVoucherService>();
-            services.AddScoped<EmployeeService>();
-            services.AddScoped<EmployeeShifteService>();
-            services.AddScoped<FloorService>();
-            services.AddScoped<MenuItemService>();
-            services.AddScoped<MenuService>();
-            services.AddScoped<OrderItemService>();
-            services.AddScoped<OrderService>();
-            services.AddScoped<ProductService>();
-            services.AddScoped<ReservationService>();
-            services.AddScoped<RewardPointeService>();
-            services.AddScoped<RoleAccountService>();
-            services.AddScoped<RoleService>();
-            services.AddScoped<ShifteService>();
-            services.AddScoped<TableService>();
-            services.AddScoped<UnitService>();
-            services.AddScoped<VoucherService>();
-            services.AddSingleton<NetworkService>();
-            services.AddDbContext<AppDBContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnect"))
-            );
+            var key = Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]);
 
-
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+                optionos =>
+                {
+                    optionos.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(key)
+                    };
+                });
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                
+            })
+            .AddCookie();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
