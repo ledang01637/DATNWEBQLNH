8819ef2c86b14008aa64f6618a39325d2c06d8f4
DangLH <ledang01637@gmail.com>
2024-10-23 09:48:38 +0700

    Update request table

--- DATN/DATN/Client/Pages/Demo.razor.cs
+++ DATN/DATN/Client/Pages/Demo.razor.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.SignalR.Client;
+﻿using DATN.Shared;
+using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 
@@ -9,6 +13,10 @@ namespace DATN.Client.Pages
     {
         private HubConnection hubConnection;
         private string message;
+        private int searchTerm = 0;
+        private bool showModal = false;
+        private List<Table> tables = new();
+        private List<Floor> floors = new();
 
         protected override async Task OnInitializedAsync()
         {
@@ -16,7 +24,10 @@ namespace DATN.Client.Pages
                 .WithUrl(Navigation.ToAbsoluteUri("/ProcessHub"))
                 .Build();
 
+            tables = await httpClient.GetFromJsonAsync<List<Table>>("api/Table/GetTable");
+            floors = await httpClient.GetFromJsonAsync<List<Floor>>("api/Floor/GetFloor");
             await hubConnection.StartAsync();
+
         }
 
         private async Task ProcessPayment()
@@ -27,10 +38,31 @@ namespace DATN.Client.Pages
             }
             else
             {
-                await JS.InvokeVoidAsync("alert", "Không thể kết nối tới server!");
+                await JS.InvokeVoidAsync("howAlert", "Không thể kết nối tới server!");
             }
         }
 
+        private void CompleteTableRequest(RequestTable table)
+        {
+            table.IsCompleted = true;
+            showModal = true;
+        }
+
+        private void CloseModal()
+        {
+            showModal = false;
+        }
+
+        // Function to filter tables based on search term
+        private IEnumerable<RequestTable> FilterTables(IEnumerable<RequestTable> requestTables)
+        {
+            if (searchTerm == 0)
+            {
+                return requestTables;
+            }
+
+            return requestTables.Where(t => t.NumberTable == searchTerm);
+        }
 
         public async ValueTask DisposeAsync()
         {
---
+++ DATN/DATN/Shared/RequestTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DATN.Shared
+{
+    public class RequestTable
+    {
+        public int NumberTable { get; set; }
+        public List<string> Requests { get; set; } = new();
+        public bool IsCompleted { get; set; } = false;
+        public string ButtonClass => IsCompleted ? "completed" : "processing";
+    }
+}
