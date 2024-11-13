ebdb53f03e76456684781c846ca75b2149ad6419
nghiaDEV <149445948+nghiamhhpc05062@users.noreply.github.com>
2024-11-13 19:37:20 +0700

    cherrypick nghĩa

---
+++ DATN/DATN/Client/Pages/AdminManager/AdminShifte.razor.cs
@@ -0,0 +1,101 @@
+﻿
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+using System.Net.Http.Json;
+using DATN.Shared;
+using Microsoft.AspNetCore.Components;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace DATN.Client.Pages.AdminManager
+{
+    public partial class AdminShifte
+    {
+        private List<DATN.Shared.Shifte> listShifte = new List<DATN.Shared.Shifte>();
+        private List<DATN.Shared.Shifte> filter = new List<DATN.Shared.Shifte>();
+        private bool isLoaded = false;
+        private string errorMessage;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadShiftes();
+            isLoaded = true;
+        }
+
+        private async Task LoadShiftes()
+        {
+            try
+            {
+                listShifte = await httpClient.GetFromJsonAsync<List<DATN.Shared.Shifte>>("api/Shifte/GetShifte");
+                filter = listShifte;
+            }
+            catch (Exception ex)
+            {
+                errorMessage = $"Error loading unit: {ex.Message}";
+            }
+        }
+
+        private async Task HideShifte(int shifteId)
+        {
+            try
+            {
+                var shifte = listShifte.FirstOrDefault(p => p.Shifte_Id == shifteId);
+                if (shifte != null)
+                {
+                    shifte.IsDeleted = true;
+                    await httpClient.PutAsJsonAsync($"api/Shifte/{shifteId}", shifte);
+                    await LoadShiftes();
+                    StateHasChanged();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error hiding voucher: {ex.Message}");
+            }
+        }
+
+        private async Task RestoreShifte(int shifteId)
+        {
+            try
+            {
+                var shifte = listShifte.FirstOrDefault(p => p.Shifte_Id == shifteId);
+                if (shifte != null)
+                {
+                    shifte.IsDeleted = false;
+                    await httpClient.PutAsJsonAsync($"api/Shifte/{shifteId}", shifte);
+                    await LoadShiftes();
+                    StateHasChanged();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Đã xảy ra lỗi khi khôi phục : {ex.Message}");
+            }
+        }
+
+        private void EditShifte(int shifteId)
+        {
+            Navigation.NavigateTo($"/editshifte/{shifteId}");
+        }
+
+        private void CreateShifte()
+        {
+            Navigation.NavigateTo($"/createshifte");
+        }
+
+        private void Filter(ChangeEventArgs e)
+        {
+            var searchTerm = e.Value.ToString().ToLower();
+            filter = string.IsNullOrWhiteSpace(searchTerm)
+                ? listShifte
+                : listShifte.Where(p => p.Shifte_Name.ToLower().Contains(searchTerm)).ToList();
+        }
+
+
+
+    }
+}
+
+
